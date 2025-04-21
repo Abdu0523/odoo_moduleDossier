7fc063e40b96d017b27a275a4995bc58fafa5e8f
ndiaye <ndiayeabdoundour@proton.me>
2025-04-21 10:29:41 +0100

    reprise sur vscode

--- __manifest__.py
+++ __manifest__.py
@@ -1,12 +1,11 @@
 {
-    'name': 'somasic_dossier',
+    'name': 'Dossier',
     'version': '1.0',
     'summary': 'Gestion des dossiers clients',
     'author': 'Abdou Ndour Ndiaye',
     'category': 'Gestion',
     'depends': ['base', 'contacts', 'mail', 'sale', 'account'],
     'data': [
-        'security/dossier_security.xml',
         'security/ir.model.access.csv',
         'views/dossier_views.xml',
         'views/dossier_menus.xml',
